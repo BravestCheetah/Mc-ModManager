290f12253b8743f461c895e1ad7b04524380df08
BravestCheetah <luckedev888@outlook.com>
2025-01-04 16:45:22 +0100

    cool

--- mc-modmanager.py
+++ mc-modmanager.py
@@ -5,6 +5,29 @@ import os
 import glob
 import math
 import sys
+import zipfile
+import json
+import requests
+from packaging.version import Version
+
+
+
+def get_version_data():
+    versions_data_url = 'https://launchermeta.mojang.com/mc/game/version_manifest_v2.json'
+
+    response = requests.get(versions_data_url)
+
+    version_data = response.json()
+
+    version_names = []
+
+    for version in version_data["versions"]:
+        if version["type"] == "release":
+            version_names.append(version["id"])
+
+    return version_names
+
+
 
 mods = []
 
@@ -32,6 +55,46 @@ def set_window_icon(icon_name, root):
         print(f"Error: Icon file not found at {icon_path}")
 
 
+from packaging.version import Version
+
+
+from packaging import version
+
+from packaging import version
+
+def normalize_version(constraint):
+    """
+    Normalize version constraints (e.g., '>=1.21, <2' -> separate checks)
+    """
+    parts = [part.strip() for part in constraint.split(',')]
+    conditions = []
+
+    for part in parts:
+        if '>' in part:
+            conditions.append(lambda v: v > version.parse(part[2:].strip()))
+        elif '<' in part:
+            conditions.append(lambda v: v < version.parse(part[2:].strip()))
+        elif '=' in part:
+            conditions.append(lambda v: v == version.parse(part[2:].strip()))
+        else:
+            conditions.append(lambda v: v == version.parse(part))
+
+    return conditions
+
+def get_valid_versions(mod_version, constraint):
+    """
+    Check if mod_version satisfies the version constraint.
+    """
+    try:
+        mod_version = version.parse(mod_version)
+        conditions = normalize_version(constraint)
+
+        # Check if the mod version satisfies all conditions
+        return all(condition(mod_version) for condition in conditions)
+    except Exception as e:
+        print(f"Error comparing versions: {mod_version} with {constraint}: {e}")
+        return False
+
 
 
 class ModData:
@@ -41,18 +104,50 @@ class ModData:
 
     for mod in modfiles_in_system:
         new_mod = mod[:-4]
-        mods_in_system.append(new_mod)
+
+        with zipfile.ZipFile(mod, 'r') as f:
+
+            try:
+                list_of_mod_content = f.infolist()
+
+                for i in list_of_mod_content:
+
+                    file_name = i.filename
+                    if file_name == "fabric.mod.json":
+
+                        with f.open(file_name, "r") as json_file:
+                            data = json.load(json_file)
+
+                        depends = data.get("depends", {})
+                        minecraft_version = depends.get("minecraft")
+
+                        if minecraft_version:
+                            print(f"{mod}: Minecraft version dependency: {minecraft_version}")
+                            compatible_versions = get_valid_versions(minecraft_version, get_version_data())
+                            print(f"Compatible versions: {compatible_versions}")
+                        else:
+                            print(f"{mod}: No 'minecraft' dependency found.")
+
+            except Exception as e:
+                print(f"An error occured while loading mods: {e}")
+
+        mod_name = mod.split(os.path.sep)[-1]
+        mods_in_system.append(mod_name)
+
+    print("Mods in system:", mods_in_system)
 
 
 
 def delete_files_in_mc_mods_folder():
     try:
         files = glob.glob(os.path.join(os.path.join(minecraft_launcher_lib.utils.get_minecraft_directory(), "mods"), "*.jar"))
+        print(files)
         for file in files:
             os.remove(file)
+            print(file)
 
-    except OSError as e:
-        change_status("An error occured when clearing mod folder.")
+    except Exception as e:
+        change_status(f"An error occured when clearing mod folder: {e}")
 
 
 
@@ -82,10 +177,11 @@ def mod_select():
 
     for mod in ModData.mods_in_system:
         mod_id += 1
+        mod_name = mod[0]
         mods.append(IntVar(value=0))  # Add a new IntVar for each mod
-        mod = mod.replace("-", " ")
-        mod = mod.replace("_", " ")
-        mod_check = Checkbutton(select_gui, text=mod, variable=mods[mod_id - 1])
+        mod_name = mod_name.replace("-", " ")
+        mod_name = mod_name.replace("_", " ")
+        mod_check = Checkbutton(select_gui, text=mod_name, variable=mods[mod_id - 1])
 
         mod_check.grid(row=math.floor(mod_id / 2 - 0.5) + 1, column=1 - mod_id % 2, sticky="w")
 
@@ -110,6 +206,7 @@ def inject_saved_mods():
     global mods
 
     change_status("Clearing minecraft mods folder...")
+    delete_files_in_mc_mods_folder()
 
     for index, mod_state in enumerate(mod_save):
         change_status("Checking mod: " + ModData.modfiles_in_system[index])
